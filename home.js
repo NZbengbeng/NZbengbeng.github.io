e3420c4518eb186065c3ed4f3c5f9c4b7657ff5f
Benjamin Yu <benjaminyu@wifi-staff-172-24-9-152.net.auckland.ac.nz>
2018-01-29 00:11:44 +1300

    More on website!

--- home.js
+++ home.js
@@ -30,7 +30,7 @@ generatedEl.addEventListener('animationend', () => {
 })
 
 const makeContent = () => {
-  generatedEl.innerText = helloChoices[cycleHello()] + ","
+  generatedEl.innerText = helloChoices[cycleHello()] + "!"
 }
 
 class Jilada {
@@ -44,3 +44,24 @@ class Jilada {
 }
 let jono = new Jilada('some props')
 console.log(jono.dog())
+
+function startTime()
+{
+var today=new Date();
+var h=today.getHours();
+var m=today.getMinutes();
+var s=today.getSeconds();
+// add a zero in front of numbers<10
+m=checkTime(m);
+s=checkTime(s);
+document.getElementById('txt').innerHTML=h+":"+m+":"+s;
+t=setTimeout('startTime()',500);
+}
+function checkTime(i)
+{
+if (i<10)
+{
+i="0" + i;
+}
+return i;
+}
