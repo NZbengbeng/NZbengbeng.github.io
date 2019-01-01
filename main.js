1e76c24204d44ba6d346d43a7fa3224e07283b17
nzbengbeng <yu.benjamin2@gmail.com>
2019-01-01 10:37:07 +0700

    about.html v1

---
+++ main.js
@@ -0,0 +1,13 @@
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  function openAboutSection() {
+    document.getElementById("content").innerHTML = "whatever";
+  }
+  function myDemo() {
+  document.getElementById("demo").innerHTML = "lol";
+  }
