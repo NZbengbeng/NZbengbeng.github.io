4a5bbfec536923fdd96d42232a6aa9777cd20dc6
nzbengbeng <yu.benjamin2@gmail.com>
2019-01-03 14:30:08 +0700

    updated

--- js/whiteboard.js
+++ js/whiteboard.js
@@ -65,3 +65,32 @@ function setRandomColor() {
   function myDemo() {
   document.getElementById("demo").innerHTML = "lol";
   }
+
+
+    /*countdown timer*/
+// Set the date we're counting down to
+
+//Countdown till winter olympics 2020
+var countDownDate = new Date("July 24, 2020 11:00:00 UTC").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+  // Get todays date and time
+  var now = new Date().getTime();
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
--- main.js
+++ main.js
@@ -65,3 +65,4 @@ function setRandomColor() {
   function myDemo() {
   document.getElementById("demo").innerHTML = "lol";
   }
+
